8ab58d2c5b3ce9ed5ca048053a628a9adca0fb36
Praveen <maurya.praveen@gmail.com>
2019-04-09 00:07:53 +0800

    usercontroller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::UsersController < Api::V1::BaseController
+  skip_before_action :verify_authenticity_token
+  before_action :set_user
+
+  def show
+  end
+
+  def update
+    if @user.
+  end
+
+end
