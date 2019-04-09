bba1531b4291b3689f65ab016d89849e5bf72e0b
Agnes Selvita <45679321+aggy-k@users.noreply.github.com>
2019-04-09 16:23:52 +0800

    Update goals_controller.rb

--- app/controllers/api/v1/goals_controller.rb
+++ app/controllers/api/v1/goals_controller.rb
@@ -1,18 +1,16 @@
 class Api::V1::GoalsController < Api::V1::BaseController
   skip_before_action :verify_authenticity_token
+  before_action :set_user, only: [:index, :create, :update]
+  before_action :set_goal, only: [:show, :update, :destroy]
 
   def index
-    @user = current_user
-    # @user = User.find(1)
     @goals = @user.goals
   end
 
   def show
-    @goal = Goal.find(params[:id])
   end
 
   def create
-    @user = current_user
     @goal = Goal.new(goal_params)
     if @goal.save
       render :show, status: :created
@@ -23,7 +21,6 @@ class Api::V1::GoalsController < Api::V1::BaseController
 
 
   def update
-    @goal = Goal.find(params[:id])
     if @goal.update
       render :show
     else
@@ -32,8 +29,17 @@ class Api::V1::GoalsController < Api::V1::BaseController
   end
 
   def destroy
-    @goal = Goal.find(params[:id])
     @goal.destroy
+  end
+  
+  private
+  
+  def set_user
+    @user = current_user
+  end
+  
+  def set_goal
+    @goal = Goal.find(params[:id])
   end
 
   def goal_params
