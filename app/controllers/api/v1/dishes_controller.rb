81e3815d3cf732c06d8d6e63ad70ec4755cb0bd0
Agnes Selvita <a.selvita@gmail.com>
2019-04-10 17:23:44 +0800

    tweaked dishes and meals controllers and view

--- app/controllers/api/v1/dishes_controller.rb
+++ app/controllers/api/v1/dishes_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::DishesController < Api::V1::BaseController
   skip_before_action :verify_authenticity_token
-  
+
   def index
     @dishes = Dish.all
+  end
+
+  def show
+    @dish = Dish.find(params[:id])
   end
 end
---
+++ app/views/api/v1/dishes/show.json.jbuilder
@@ -0,0 +1,6 @@
+json.extract! @dish, :id, :name
+json.nutrients @dish.doses do |dose|
+  json.name dose.nutrient.name
+  json.unit dose.nutrient.unit
+  json.extract! dose, :amount
+end
