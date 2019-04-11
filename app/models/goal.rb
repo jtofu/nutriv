9097442a70a31eead74afc3cfce0f8042ed947a4
Agnes Selvita <a.selvita@gmail.com>
2019-04-11 11:36:29 +0800

    added validations to goal model

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,8 @@
 class Goal < ApplicationRecord
   belongs_to :user
   belongs_to :nutrient
+
+  validates :amount, numericality: true
+  validates :user, uniqueness: { scope: :nutrient,
+    message: "Each nutrient is unique to each user" }
 end
