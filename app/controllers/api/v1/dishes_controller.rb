cd92719aff89d6728516e64fdfd8acf36fedcca0
Agnes Selvita <a.selvita@gmail.com>
2019-04-11 10:58:10 +0800

    updated dish controller

--- app/controllers/api/v1/dishes_controller.rb
+++ app/controllers/api/v1/dishes_controller.rb
@@ -2,7 +2,12 @@ class Api::V1::DishesController < Api::V1::BaseController
   skip_before_action :verify_authenticity_token
 
   def index
-    @dishes = Dish.all
+    if params[:search].nil?
+      @dishes = Dish.all
+    else
+      search = params[:search].downcase
+      @dishes = Dish.where("LOWER(name) LIKE ?", "%#{search}%")
+    end
   end
 
   def show
