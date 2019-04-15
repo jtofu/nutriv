1c88a54dbf2b453d5c929ce64927290016102d99
Agnes Selvita <a.selvita@gmail.com>
2019-04-15 13:43:00 +0800

    added in_time_zone and quantity to meals index view

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Nutriv
   class Application < Rails::Application
+    # config.time_zone = "Beijing"
+
     config.generators do |generate|
           generate.assets false
           generate.helper false
