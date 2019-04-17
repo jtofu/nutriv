f5a9cdb1ce4c55fdf232ef3e3738db14a12c2f86
Agnes Selvita <a.selvita@gmail.com>
2019-04-17 14:14:30 +0800

    commit changes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,11 +3,54 @@ class Api::V1::UsersController < Api::V1::BaseController
   before_action :set_user
 
   def show
+    @last7d_cal = []
+    today = Date.today.to_date
+    @sum_protein = 0
+    @sum_fat = 0
+
+    @user.meals.each do |meal|
+      if meal.created_at.to_date == today
+        @sum_protein += meal.dish.doses.find_by(nutrient: Nutrient.find_by(name: "Protein")).amount
+        @sum_fat += meal.dish.doses.find_by(nutrient: Nutrient.find_by(name: "Total Fat")).amount
+      end
+    end
+
+    7.times do |n|
+      day = 6 - n
+      sum_cal = 0
+
+      sum_date = today - day
+
+      @user.meals.each do |meal|
+        meal_date = meal.created_at.to_date
+        sum_cal += meal.dish.doses.find_by(nutrient: Nutrient.find_by(name: "Calories")).amount if meal_date == sum_date
+      end
+
+      @last7d_cal << sum_cal
+    end
+
+    unless @user.goals.find_by(nutrient: Nutrient.find_by(name: "Calories")).nil?
+      @goalCal = @user.goals.find_by(nutrient: Nutrient.find_by(name: "Calories")).amount
+    else
+      @goalCal = 0
+    end
+
+    unless @user.goals.find_by(nutrient: Nutrient.find_by(name: "Protein")).nil?
+      @goalProtein = @user.goals.find_by(nutrient: Nutrient.find_by(name: "Protein")).amount
+    else
+      @goalProtein = 0
+    end
+
+    unless @user.goals.find_by(nutrient: Nutrient.find_by(name: "Total Fat")).nil?
+      @goalFat = @user.goals.find_by(nutrient: Nutrient.find_by(name: "Total Fat")).amount
+    else
+      @goalFat = 0
+    end
   end
 
   def update
     if @user.update(user_params)
-      render :show
+      # render :show
     else
       render_error
     end
--- app/views/api/v1/users/show.json.jbuilder
+++ app/views/api/v1/users/show.json.jbuilder
@@ -1,6 +1,29 @@
 json.extract! @user, :id, :wechat_id, :username, :profile_image, :first_name, :last_name
+json.last7d @last7d_cal
+json.todayCal @last7d_cal[-1]
+json.todayProtein @sum_protein
+json.todayFat @sum_fat
+
+json.goalCal @goalCal
+json.goalProtein @goalProtein
+json.goalFat @goalFat
+
 json.goals @user.goals do |goal|
   json.extract! goal, :id, :amount
   json.unit goal.nutrient.unit
   json.name goal.nutrient.name
+end
+
+json.meals @user.meals do |meal|
+  json.extract! meal, :id, :quantity
+  json.date meal.created_at.in_time_zone("Beijing").strftime('%Y-%m-%e')
+  json.time meal.created_at.in_time_zone("Beijing").strftime('%l:%M %p')
+  json.dish meal.dish.name
+  json.dish_short meal.dish.name[0..20] + ".."
+  json.image meal.dish.image
+    json.nutrients meal.dish.doses do |dose|
+      json.name dose.nutrient.name
+      json.unit dose.nutrient.unit
+      json.extract! dose, :amount
+    end
 end
